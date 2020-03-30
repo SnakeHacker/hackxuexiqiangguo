befa6d5d110e062ccfffc1f5be1cf318acc64972
SnakeHacker <616976756@qq.com>
2020-03-31 01:58:33 +0800

    update

---
+++ Makefile
@@ -0,0 +1,5 @@
+run:
+	python3 fuck.py --debug=true
+
+install:
+	pip install -r requirements.txt
---
+++ fuck.py
@@ -0,0 +1,119 @@
+from selenium import webdriver
+import time
+from random import randint
+import argparse
+import sys
+import socket
+socket.setdefaulttimeout(30)
+
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+FUCK_URL = "https://www.xuexi.cn/"
+LOGIN_URL = 'https://pc.xuexi.cn/points/login.html'
+PAGE_NUMS = 10
+VIDEO_NUMS = 5
+
+
+def start_fuck(debug):
+    desired_capabilities = DesiredCapabilities.CHROME
+    desired_capabilities["pageLoadStrategy"] = "none"
+    chrome_options = Options()
+    if not debug:
+        # 是否展示界面，debug时候用得到
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disbale-dev-shm-usage')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    browser.implicitly_wait(20)
+    browser.get(LOGIN_URL)
+    time.sleep(15)
+
+    browser.get(FUCK_URL)
+    time.sleep(5)
+
+    handle = browser.current_window_handle
+
+    # 文本新闻
+    important_news = browser.find_elements_by_class_name("_3wnLIRcEni99IWb4rSpguK")
+    for news in important_news:
+        global PAGE_NUMS
+        if PAGE_NUMS == 0:
+            break
+        PAGE_NUMS-=1
+
+        print(news.text)
+        news.click()
+        time.sleep(randint(2, 7))
+        scrollCount = 0
+        # 随机滑动页面
+        while scrollCount < 10:
+            handles = browser.window_handles
+            for newhandle in handles:
+                if newhandle != handle:
+                    browser.switch_to.window(newhandle)
+            jsCode = "var q=document.documentElement.scrollTop={}".format(randint(100, 700))
+            browser.execute_script(jsCode)
+            time.sleep(randint(3, 20))
+            scrollCount += 1
+        handles = browser.window_handles
+        for newhandle in handles:
+            if newhandle != handle:
+                browser.switch_to.window(newhandle)
+                browser.close()
+        browser.switch_to_window(handle)
+    
+    dypd = browser.find_element_by_xpath("//div[text()='第一频道']")
+    time.sleep(5)
+    dypd.click()
+    time.sleep(3)
+    handles = browser.window_handles
+    for newhandle in handles:
+        if newhandle != handle:
+            browser.switch_to.window(newhandle)
+    # 视频新闻
+    video_news = browser.find_elements_by_class_name("textWrapper")
+    for news in video_news:
+        global VIDEO_NUMS
+        if VIDEO_NUMS == 0:
+            break
+        VIDEO_NUMS -= 1
+
+        print(news.text)
+        news.click()
+
+        time.sleep(randint(2, 7))
+        scrollCount = 0
+        # 随机滑动页面
+        while scrollCount < 10:
+            handles = browser.window_handles
+            for newhandle in handles:
+                if newhandle != handle:
+                    browser.switch_to.window(newhandle)
+            jsCode = "var q=document.documentElement.scrollTop={}".format(randint(100, 300))
+            browser.execute_script(jsCode)
+            time.sleep(randint(3, 20))
+            scrollCount += 1
+        handles = browser.window_handles
+        for newhandle in handles:
+            if newhandle != handle:
+                browser.switch_to.window(newhandle)
+                browser.close()
+        browser.switch_to_window(handle)
+
+    time.sleep(10)
+    print("今日份已刷完")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', type=bool, default=False)
+
+    args = parser.parse_args()
+
+    debug = args.debug
+    start_fuck(debug)
